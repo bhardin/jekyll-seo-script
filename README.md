40e0bc663e9f0d4000f997e03b8f36b92d1ac04c
Brett Hardin <brett@sourceninja.com>
2012-06-26 11:07:21 -0700

    Adding all functionality

---
+++ README.md
@@ -0,0 +1,38 @@
+SEO Jekyll tool
+===============
+
+One problem that I have with jekyll, and I missed from wordpress, was a way to analyze posts for SEO optimization. I borrowed a lot of the work from Wordpress SEO by YOAST.
+
+Usage
+-----
+Download the jekyll-seo.rb file and run it against the output of jekyll. This is important. You cannot run this on the markdown file, because it does not contain the HTML that is required for SEO analysis.
+
+	jekyll-seo -k "optimize jekyll for seo" _site/2012/06/open-source-attribution/index.html
+
+Output
+------
+
+```
+Analyzing Post: _site/2012/06/open-source-attribution/index.html...
+
+Article Heading: true (2)
+Page title: true (1)
+Content: true (12)
+Meta description: true (1)
+```
+
+TODO
+----
+* Identify SEO terms in the URL
+* You have not used your keyword / keyphrase in any subheading (such as an H2) in your copy.
+* No images appear in this page, consider adding some as appropriate.
+* The meta description is under 120 characters, however up to 156 characters are available. The available space is shorter than the usual 155 characters because Google will also include the publication date in the snippet.
+* The page title contains keyword / phrase, but it does not appear at the beginning; try and move it to the beginning.
+* The keyword / phrase does not appear in the URL for this page. If you decide to rename the URL be sure to check the old URL 301 redirects to the new one!
+* The copy scores 68.9 in the Flesch Reading Ease test, which is considered OK to read.
+* This page has 14 outbound link(s).
+* The keyword density is 1.74%, which is great, the keyword was found 15 times.
+* The meta description contains the primary keyword / phrase.
+* There are 890 words contained in the body copy, this is greater than the 300 word recommended minimum.
+* The page title is more than 40 characters and less than the recommended 70 character limit.
+#The keyword appears in the first paragraph of the copy.
--- jekyll-seo.rb
+++ jekyll-seo.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 # Purpose: A simple script to identify SEO problems in jekyll blog posts
 # License: http://github.com/bhardin/jekyll-seo/license.txt
-# I mimiced a lot of functionaltiy from Wordpress SEO by YOAST
-
 
 require 'optparse'
+require 'nokogiri'
+
+heading = [];
+title = [];
+url = [];
+content = [];
+meta_description = [];
+temp = [];
+
 options = {}
 
 optparse = OptionParser.new do|opts|
 	# Set a banner, displayed at the top of the help screen.
-	opts.banner = "Usage: ./script -k keywords FILE"
+	opts.banner = "Usage: ./jekyll-seo -k keywords FILE"
 
 	# Define the options, and what they do
 	options[:verbose] = false
@@ -34,30 +41,74 @@ optparse = OptionParser.new do|opts|
 end
 
 optparse.parse!
-if options[:keyword] == nil or options[:post] == nil
-	puts "Usage: ./script -k keywords -p post"
-	exit
-end
-	
+
 puts "Being verbose" if options[:verbose]
-if options [:verbose]
+if options[:verbose]
 	puts "keyword: #{options[:keyword]}" if options[:keyword]
-	puts "analyzing post: #{options[:post]}" if options[:post]
+	#puts "analyzing post: #{options[:post]}" if options[:post]
 end
  
 ARGV.each do|f|
-	puts "Resizing image #{f}..."
+	puts "Analyzing Post: #{f}..."
+	post = Nokogiri::HTML(open(f))
+
+	# Search for keyword in heading
+	post.css('h1').each do |this|
+		if options[:verbose]
+			puts "heading found"
+			puts "heading: #{this}"
+		end
+
+		heading = this.to_s.scan(/#{options[:keyword]}/i)
+
+		if options[:verbose]
+			puts "heading-found: #{title}"
+		end
+	end
+
+	# Search for keyword in title
+	post.css('title').each do |this|
+		if options[:verbose]
+			puts "title found"
+			puts "title: #{this}"
+		end
+
+		title = this.to_s.scan(/#{options[:keyword]}/i)
+
+		if options[:verbose]
+			puts "title-found: #{title}"
+		end
+	end
+
+	# Search for keyword in body
+	post.css('body').each do |this|
+		if options[:verbose]
+			puts "content found"
+			puts "content: #{this}"
+		end
+
+		content = this.to_s.scan(/#{options[:keyword]}/i)
+	end
+
+	# Search for keyword in Meta Description
+	post.css('meta').each do |this|
+		if this.attribute("name").to_s == "description"
+			if options[:verbose]
+				puts "Meta Description found"
+				puts "Meta Description: #{this.attribute('content')}"
+			end
+
+			meta_description = this.attribute("content").to_s.scan(/#{options[:keyword]}/i)
+		end
+	end
 end
 
-@keyword ="seo"
-@post = "my-post"
-
 puts ""
-puts "Article Heading: Yes (1)"
-puts "Page title: Yes (1)"
-puts "Page URL: Yes (1)"
-puts "Content: Yes (15)"
-puts "Meta description: Yes (1)"
+puts "Article Heading: #{not heading.empty?} (#{heading.count})"
+puts "Page title: #{not title.empty?} (#{title.count})"
+# puts "Page URL: Yes (1)"
+puts "Content: #{not content.empty?} (#{content.count})"
+puts "Meta description: #{not meta_description.empty?} (#{meta_description.count})"
 
 #You have not used your keyword / keyphrase in any subheading (such as an H2) in your copy.
 #No images appear in this page, consider adding some as appropriate.
@@ -71,4 +122,3 @@ puts "Meta description: Yes (1)"
 #There are 890 words contained in the body copy, this is greater than the 300 word recommended minimum.
 #The page title is more than 40 characters and less than the recommended 70 character limit.
 #The keyword appears in the first paragraph of the copy.
-
